9fec7af01a3c95789aa596ee9125320e1680ddcb
Hsalom289 <hsalom289@gmail.com>
2025-08-02 23:50:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ ADDITIONAL_GROUPS = [
 def is_working_time():
     """23:00 dan 03:00 gacha ish vaqti (hozirgi vaqtga moslashtirildi)"""
     now = datetime.now().time()
-    return dt_time(19, 0) <= now or now <= dt_time(4, 0)
+    return dt_time(5 , 0) <= now or now <= dt_time(18 , 0)
 
 async def get_admin_groups():
     """Admin bo‘lgan guruhlar va qo‘shimcha guruhlar"""
@@ -156,4 +156,5 @@ async def main():
 if __name__ == "__main__":
     with client:
         client.loop.run_until_complete(main())
+
 
